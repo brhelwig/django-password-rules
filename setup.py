e19047d2e31f2f312a13bc857129b0d3190629da
Brandon Helwig <github@brandonhelwig.com>
2023-04-08 00:34:12 -0600

    pre-commit updates

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ setup(
     version=__import__("passwords").__version__,
     author="Donald Stufft",
     author_email="donald@e.vilgeni.us",
-    description=("A Django reusable app that provides validators and a form "
-                 "field that checks the strength of a password"),
+    description=(
+        "A Django reusable app that provides validators and a form "
+        "field that checks the strength of a password"
+    ),
     long_description=open("README.rst").read(),
     url="http://github.com/dstufft/django-passwords/",
     license="BSD",
@@ -14,10 +16,10 @@ setup(
         "passwords",
     ],
     include_package_data=True,
-    install_requires = [
+    install_requires=[
         "Django >= 1.3",
     ],
-    classifiers = [
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
